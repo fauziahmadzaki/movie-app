42ae7375f053b301475e671df98f0b0ef3a0710a
fauziahmadzaki <fauziahmadzaki7@gmail.com>
2025-07-27 04:01:18 +0700

    Change the about text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,12 +7,26 @@ export const AboutPage = () => {
       <div className="bg-neutral-900 min-h-screen flex flex-col pt-20 items-center gap-5">
         <h1 className="text-4xl font-bold text-yellow-400">About Us</h1>
         <p className="text-lg font-semibold text-center text-white w-6/10">
-          Layar kaca selikur are website that provide information about various
-          movies around the world. We also provide information about the movie's
-          genre, release date, and rating. The data that displayed on this
-          website is using API from TMDB.com. 
+          Layar Kaca Selikur is a website that provides information about
+          various movies from around the world. We offer details such as genres,
+          release dates, and ratings for each film. All the data displayed on
+          this website is sourced through the API provided by TMDB. Our goal is
+          to help movie enthusiasts discover and explore films from different
+          countries and cultures, all in one place. Whether you're searching for
+          the latest blockbuster or a hidden gem, we've got you covered. We are
+          continuously updating our content to ensure you get the most accurate
+          and up-to-date movie information available.
         </p>
-        <p className="text-2xl font-bold text-white text-center">Credit for API : <Link className="underline" target="_blank" to={'https://www.themoviedb.org'}>https://www.themoviedb.org</Link></p>
+        <p className="text-2xl font-bold text-white text-center">
+          Credit for API :{" "}
+          <Link
+            className="underline"
+            target="_blank"
+            to={"https://www.themoviedb.org"}
+          >
+            https://www.themoviedb.org
+          </Link>
+        </p>
       </div>
     </div>
   );
